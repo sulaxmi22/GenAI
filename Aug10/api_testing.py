13db010c61867546aeb9d9e90af85974ad498bed
sulaxmi22 <sulaxmi.raskar22@gmail.com>
2025-08-17 23:56:59 -0500

    commit render error

--- Aug10/api_testing.py
+++ Aug10/api_testing.py
@@ -14,9 +14,13 @@ class User(BaseModel):
     age: int
 
 #app - fast api variable to expose api
-@app.get("/users")
-def print_users():
-    return user_db
+# @app.get("/users")
+# def print_users():
+#     return user_db
+
+@app.get("/")
+async def root():
+    return {"status": "ok"}
 
 @app.put("/users/v1/update/{user_id}")
 def user_update(user_id: int, user_details: User):
